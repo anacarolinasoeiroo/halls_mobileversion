9cea4261b3bcfc11e7001b9a467519cf1519270c
anacarolinasoeiroo <anacarolinasoeiro@live.com.pt>
2021-08-02 20:18:05 +0100

    commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.slider').slick({
+        infinite: false,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        dots: false,
+        prevArrow: false,
+        nextArrow: false,
+    });
+  });
